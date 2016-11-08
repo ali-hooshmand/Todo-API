bb9da9582ba30f03970eaf5d3a35f732e0b7b374
Ali Hooshmand <hooshmand.ali@gmail.com>
2016-11-07 19:24:04 -0800

    Add auth middleware and findByToken class method

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcryptjs');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -16,7 +17,7 @@ app.get('/', function (req, res) {
 });
 
 // GET /todos?completed=false&q=work
-app.get('/todos', function (req, res) {
+app.get('/todos', middleware.requireAuthentication, function (req, res) {
     var query = req.query;
     var where = {};
 
@@ -59,7 +60,7 @@ app.get('/todos', function (req, res) {
 });
 
 // GET /todos/:id
-app.get('/todos/:id', function (req, res) {
+app.get('/todos/:id', middleware.requireAuthentication, function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	//var matchedTodo = _.findWhere(todos, {id: todoId});
     db.todo.findById(todoId).then(function(todo){
@@ -79,7 +80,7 @@ app.get('/todos/:id', function (req, res) {
 });
 
 // POST /todos
-app.post('/todos', function (req, res) {
+app.post('/todos', middleware.requireAuthentication, function (req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 db.todo.create(body).then(function(todo){
     res.json(todo.toJSON());
@@ -99,7 +100,7 @@ db.todo.create(body).then(function(todo){
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', function (req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
     db.todo.destroy({
         where:{
@@ -131,7 +132,7 @@ app.delete('/todos/:id', function (req, res) {
 });
 
 // PUT /todos/:id
-app.put('/todos/:id', function (req, res) {
+app.put('/todos/:id', middleware.requireAuthentication, function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
     var body = _.pick(req.body, 'description', 'completed');
 	var attributes = {};
